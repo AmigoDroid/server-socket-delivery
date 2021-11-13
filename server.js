7b903323c488d034cef03914d6418fc92aa1e7a8
AmigoDroid <colteman05@gmail.com>
2021-11-13 17:35:22 -0300

    parei visão cansada

--- server.js
+++ server.js
@@ -1,15 +1,53 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const http = require('http').createServer(app);
+
+
+const io = require('socket.io')(http);
+const qr = require('qr-image');
+
+
+function idA(){
+    const idN = Math.floor(Math.random()*50**5);
+    return idN;
+}
+
+const data = new Date();
+let dia = data.getDate();
+let mes = data.getMonth();
+let ano = data.getFullYear();
+
 const porta  = process.env.PORT||8080;
 
+io.on('connection',(cliente)=>{
+    console.log("ID: "+cliente.id);
+   cliente.on('info_Cliente',(info)=>{
+       console.log(info.nome);
+   })
+
+   cliente.on('online',(status)=>{
+       console.log(status);
+   })
 
 
+})
+app.get('/',(req,res)=>{
+    const obj = {
+        id:idA(),
+        nome:'nome',
+        data:`${dia}/${mes}/${ano}`
+    }
+    const code = qr.image(JSON.stringify(obj),{type:'svg'});
+    res.type('svg');
+    console.log(obj.id);
+    code.pipe(res);
+})
+
+app.get('/home',(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
 
 
-server.listen(3000,()=>{
-console.log(location.host+""+porta);
+http.listen(porta,()=>{
+console.log("Rodando na porta: "+porta);
 })

a9e55dc0d967b95966c373bc06f875af27c1adeb
AmigoDroid <colteman05@gmail.com>
2021-12-05 18:06:16 -0200

    refatorando

--- server.js
+++ server.js
@@ -1,70 +1,38 @@
-const express = require('express');
+
+const express = require('express')
+const path  = require('path');
+
+
 const app = express();
-const http = require('http').createServer(app);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+const PORT = process.env.PORT;
 
-const io = require('socket.io')(http);
-const qr = require('qr-image');
+app.use(express.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname,"public"));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','html');
 
+//armazaenar as mensagens
+var datamsg =[];
 
-function idA(){
-    const idN = Math.floor(Math.random()*50**5);
-    return idN;
-}
-//testar commit
-const data = new Date();
-let dia = data.getDate();
-let mes = data.getMonth();
-let ano = data.getFullYear();
-let dataCompleta=`${dia}/${mes}/${ano}`;
+    io.on('connection', socket =>{
 
-const porta  = process.env.PORT||8080;
-var msg ='';
-const dataProsses=[];
+        console.log('conectado: id='+socket.id);
+        
+        });
 
-io.on('connection',(cliente)=>{
-
-    console.log("ID: "+cliente.id);
-   cliente.broadcast.emit('allPedidos',dataProsses);
-
-   cliente.on('pedido',(quem)=>{
-       //salvar o pedido no tempo de execução
-       dataProsses.push(quem);
-       console.log(quem);
     
-       cliente.broadcast.emit('allPedidos',dataProsses);
-
-   })
 
 
-   cliente.on('disconnect',(ig)=>{
-       console.log('desconectado: '+ig.id);
-   })
-
-
-})
-app.get('/QRcode',(req,res)=>{
-    const obj = {
-        id:idA(),
-        nome:'nome',
-        data:`${dia}/${mes}/${ano}`
-    }
-    const code = qr.image(JSON.stringify(obj),{type:'svg'});
-    res.type('svg');
-    console.log(obj.id);
-    code.pipe(res);
+app.get('/',(req,res)=>{
+    res.render('index');
 });
 
-app.get('/home/:id',(req,res)=>{
-    const id = req.params.id;
-    res.sendFile(__dirname+"/index.html");
-});
-app.get('/cliente/:id',(req,res)=>{
-    const id = req.params.id;
-    res.sendFile(__dirname+'/cliente.html');
+//ouvindo na porta 3000
+server.listen(PORT||5500,()=>{
+    console.log('rodando na porta 5500');
 });
 
 
-http.listen(porta,()=>{
-console.log("Rodando na porta: "+porta);
-});

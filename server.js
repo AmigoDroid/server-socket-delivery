d7c13e60bf45db1e6e63f730c1a4ebce236b7d4d
AmigoDroid <canalmeuovo36@gmail.com>
2022-02-22 22:17:32 -0300

    update teste

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server,{cors:{origin:"*"}});
 const jwt = require('jsonwebtoken');
 const secret = 'JLAcessoriosToken';
+
 const verify = require('./verify');
 
 const PORT = process.env.PORT;
@@ -17,31 +18,54 @@ app.set('views',path.join(__dirname,"public"));
 app.engine('html',require('ejs').renderFile);
 app.set('view engine','html');
 
-//armazaenar as mensagens
-var datamsg =[];
-
     io.on('connection', socket =>{
 
         console.log('conectado: id='+socket.id);
         socket.emit('teste','Conected Server');
 
         socket.on('pedidos',(objs)=>{
+
+            console.log('novo pedido!');
            
-            console.log(objs);
-            io.emit('Pedidos',objs);
+            
+            //verificando pedido
+            const verifyResult = verify.verificar(objs);
+         
+            //verificando resultado do pedido
+            if(verifyResult){
+                pedidoFeito(true,objs.token)
+                io.emit("ConectId:"+objs.idLoja,objs)
+            }else{
+                pedidoFeito(false,objs.token)
+            }
+
         });
         
         });
-        
+        //respondendo para o cliente o resultado do pedido
+        function pedidoFeito(estado,token){
+
+            const resposta = {
+                estado:estado,
+                msgV:"pedido Realizado com sucesso",
+                msgF:"Ocorreu um Erro!"
+            }
+            io.emit(token,resposta);
+            console.log('============================');
+        }
+
+
+
+
 
 app.get('/home/:id',(req,res)=>{
     const id = req.params.id;
     res.render('index');
 });
 
-//ouvindo na porta 3000
+//ouvindo na porta 5500
 server.listen(PORT||5500,()=>{
-    console.log('rodando na porta 5500');
+    console.log('rodando na porta '+PORT);
 });
 
 
--- verify.js
+++ verify.js
@@ -1,5 +1,34 @@
+const jwt = require('jsonwebtoken');
+const secret = 'chave-58242660'
 module.exports ={
      teste() {
-         console.log('olá mundo');
+         console.log('Testando Server...');
+     },
+     verificar(obj){
+         console.log('Verificando...');
+         let res = false;
+         const {id,nome,valor,rua,numero,idLoja,token} = obj;
+         if(this.verifyToken(token)){
+             //
+             console.log('pedido Aprovado!');
+             return true;
+
+         }else{
+             //loja invalida
+             console.log('pedido Reprovado!');
+             return false;
+         }
+     },
+     verifyToken(token){
+         let retorno = false;
+          jwt.verify(token,secret,(err,decoded)=>{
+              if(err){
+                  console.log('token Invalido!');
+              }else{
+                  console.log('token Valido!')
+                 retorno = true;
+              };
+          });
+          return retorno;
      }
 }
